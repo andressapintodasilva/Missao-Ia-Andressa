2017b4c920773a08f20f2014e0c12c614e37b642
andressapintodasilva <146196237+andressapintodasilva@users.noreply.github.com>
2025-09-23 14:32:37 -0300

    23

--- main.js
+++ main.js
@@ -5,67 +5,68 @@
 
 const perguntas = [
     {
-        enunciado: "Pergunta?",
+        enunciado: "Com que frequência você sente ansiedade em seu dia a dia?",
         alternativas: [
+
             {
-                texto: " Texto/resposta",
-                afirmacao: " Afirmação ",
+                texto: " Raramente",
+                afirmacao: " Raramente a ansiedade te domina, você cultiva presença e gratidão para manter sua paz. ",
             },
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: " Frequentemente ",
+                afirmacao: " Quando a ansiedade chega, você respira, aceita o que sente e lembra que tem recursos para  se acalmar ",
             }
         ]
     },
     {
-        enunciado: " Pergunta?",
+        enunciado: " Quando você se sente ansioso, você tem alguma estratégia ou hábito que tenta praticar para se acalmar?",
         alternativas: [
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: " Eu foco na respiração, fazendo-a de forma lenta e controlada, e também escrevo sobre meus sentimentos para processá-los. ",
+                afirmacao: " Essa abordagem demonstra uma maneira eficaz de lidar com a ansiedade, pois combina técnicas de relaxamento físico, como a respiração controlada, com a expressão emocional, por meio da escrita. ",
             },
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: " Eu saio para caminhar e tento me concentrar no momento presente, usando os sentidos para me acalmar, além de recorrer a um aplicativo de meditação para relaxar rapidamente. ",
+                afirmacao: " Caminhar com atenção e meditar trazem calma e equilíbrio imediato. ",
             }
         ]
     },
     {
-        enunciado: " Pergunta?",
+        enunciado: " A ansiedade pode causar sintomas físicos, como palpitações ou falta de ar?",
         alternativas: [
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: " Não, os sintomas da ansiedade são apenas emocionais. ",
+                afirmacao: " Palpitações, sudorese, tontura e tensão muscular são manifestações físicas comuns da ansiedade, resultantes da ativação do sistema nervoso. ",
             },
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: " Sim, o corpo reage à ansiedade como se estivesse em perigo real. ",
+                afirmacao: " O corpo reage à ansiedade como se estivesse em perigo real para enfrentar ou fugir de uma ameaça, o que explica os sintomas físicos como batimentos cardíacos acelerados, respiração ofegante e tensão muscular.-",
             }
         ]
     },
     {
-        enunciado: " Pergunta?",
+        enunciado: " A ansiedade interfere no seu trabalho ou estudos?",
         alternativas: [
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: " Sim ",
+                afirmacao: " Quando a ansiedade interferir no seu trabalho ou estudos, busque estratégias para retomar o equilíbrio. ",
             },
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: " Não",
+                afirmacao: " Como a ansiedade não atrapalha nas suas atividades, aproveite esse estado de tranquilidade para manter meu foco e produtividade.  ",
             }
         ]
     },
     {
-        enunciado: " Pergunta?",
+        enunciado: " Práticas como meditação e exercícios de respiração podem realmente ajudar a controlar a ansiedade?",
         alternativas: [
             {
-                texto: " Texto/resposta ",
-                afirmacao: "Afirmação",
+                texto: " Não, elas são ineficazes para sintomas de ansiedade. ",
+                afirmacao: "A afirmação de que meditação e exercícios de respiração são ineficazes é falsa. Na verdade, essas práticas são ferramentas valiosas e acessíveis para o gerenciamento da ansiedade. ",
             },
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: " Sim, elas ajudam a acalmar o sistema nervoso e a focar a mente. ",
+                afirmacao: " Ao focar a atenção na respiração e no momento presente, essas práticas ajudam a reduzir a frequência cardíaca, a pressão arterial e a liberar a tensão muscular, combatendo os sintomas físicos da ansiedade. ",
             }
         ]
     },
