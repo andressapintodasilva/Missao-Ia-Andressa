12f52b515a1466d103703ce0b1d26837554153c7
andressapintodasilva <146196237+andressapintodasilva@users.noreply.github.com>
2025-10-07 14:53:35 -0300

    09

--- main.js
+++ main.js
@@ -82,4 +82,11 @@ function mostraPergunta() {
     caixaPerguntas.textContent = perguntaAtual.enunciado;
 }
 
+function mostraAlternativas() {
+  for (const alternativa of perguntaAtual.alternativas) {
+    const botaoAlternativas = document.createElement("button");
+    botaoAlternativas.textContent = alternativa;
+    caixaAlternativas.appendChild(botaoAlternativas);
+  }
+}
 mostraPergunta();
