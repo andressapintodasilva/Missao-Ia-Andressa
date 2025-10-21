fc1a34ff440e155fcb41cb103d2934fbc5f0d1fc
andressapintodasilva <146196237+andressapintodasilva@users.noreply.github.com>
2025-10-21 13:46:34 -0300

    21

--- main.js
+++ main.js
@@ -83,12 +83,13 @@ function mostraPergunta() {
 }
 
 function mostraAlternativas() {
-  for (const alternativa of perguntaAtual.alternativas) {
-    const botaoAlternativas = document.createElement("button");
-    botaoAlternativas.textContent = alternativa.texto;
-    caixaAlternativas.appendChild('click',function(){
-        
-    });
-  }
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener('click', function () {
+            atual++;
+            mostraPergunta();
+        })
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
 }
-mostraPergunta();
